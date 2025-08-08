11b2bcf5a2f22454ecd6ce8ac7b7ca7f368f4b26
Esteban Mundo <estebanmundo1999@gmail.com>
2025-08-07 23:25:58 -0400

    Setup sample project

---
+++ src/main/test.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk() # Instantiate instance of window
+window.geometry("420x420")
+window.title("Json Editor GUI")
+
+
+label = Label(window, text="Testing")
+label.pack()
+
+window.mainloop() # place window and listen to events
+
