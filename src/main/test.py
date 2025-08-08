e63cf9b2bacd86910beca1abe4a99d235e874c07
Esteban Mundo <estebanmundo1999@gmail.com>
2025-08-07 23:45:00 -0400

    Added file reading capability

--- src/main/test.py
+++ src/main/test.py
@@ -1,12 +1,27 @@
+import os
 from tkinter import *
+from tkinter import filedialog
 
 window = Tk() # Instantiate instance of window
 window.geometry("420x420")
 window.title("Json Editor GUI")
 
+def openFile():
+  filepath = filedialog.askopenfilename(
+    initialdir= os.getcwd(),
+    title= "Open JSON Template file",
+    filetypes= (
+                ("JSON file type","*.json"),
+                ("All files", "*.*")
+                )
+    )
+  print(filepath)
 
 label = Label(window, text="Testing")
 label.pack()
+
+button = Button(text="Open Json file", command=openFile)
+button.pack()
 
 window.mainloop() # place window and listen to events
 
